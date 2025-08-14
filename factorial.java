f82430490f29ce97dad6364b562b63a88e34cac8
Shatakshi Gupta <shatakshig2005@gmail.com>
2025-08-14 11:21:01 +0530

    progress

--- factorial.java
+++ factorial.java
@@ -2,8 +2,15 @@ package aug14;
 
 public class factorial {
     
+    public static int factorial(int n){
+        int f;
+        for(int i=1; i<=n; i++){
+            f = f*i;
 
+        }
+        return f;//factorial
+    }
     public static void main(String args[]){
-        
+
     }
 }
